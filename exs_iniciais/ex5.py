7d9561d92fe43f2bd3f4f22e9ea7f308056c1b44
Braisa <oterolemabrais@gmail.com>
2025-02-12 19:23:54 +0100

    Exercicios iniciais - Exercicio 5

--- exs_iniciais/ex5.py
+++ exs_iniciais/ex5.py
@@ -6,5 +6,12 @@ mat = np.array(((1, 1, 1, 1, 1, 10),
                 (4, 5, 6, 7, 8, 90)))
 
 print(f"A matriz ten {mat.shape[0]} filas e {mat.shape[1]} columnas.")
-print(f"A matriz ten {len(mat[0:,])} filas e {len(mat[0])} columnas.")
-# Sen funcións preexistentes?
+print(f"A matriz ten {len(mat)} filas e {len(mat[0])} columnas.")
+
+filas, elementos = 0, 0
+for i, fila in enumerate(mat):
+    filas += 1
+    for j, elem in enumerate(mat[i]):
+        elementos += 1
+columnas = elementos / filas
+print(f"A matriz ten {filas} filas e {columnas} columnas.")
