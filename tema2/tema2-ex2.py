7ce130c3de4dc9f95cb4c30cf720b228ea5c97e3
Braisa <oterolemabrais@gmail.com>
2025-02-13 19:57:22 +0100

    Tema 2 Exercicios 2 e 5 Cambios para a entrega

--- tema2/tema2-ex2.py
+++ tema2/tema2-ex2.py
@@ -5,6 +5,6 @@ func = lambda x : -.1*x**4 -.15*x**3 -.5*x**2 - .25*x + 1.2
 c = .5
 
 print("As aproximacións por diferencias finitas para a derivada numérica son as seguintes:")
-print(f"Cara adiante f'(0.5) = {(func(c+h)-func(c))/h}.")
-print(f"Cara atrás f'(0.5) = {(func(c)-func(c-h))/h}.")
-print(f"Centradas f'(0.5) = {(func(c+h)-func(c-h))/2/h}.")
+print(f"Cara adiante f'(0.5) = {(func(c+h)-func(c))/h:.4f}.")
+print(f"Cara atrás f'(0.5) = {(func(c)-func(c-h))/h:.4f}.")
+print(f"Centradas f'(0.5) = {(func(c+h)-func(c-h))/2/h:.4f}.")
--- tema2/tema2-ex5.py
+++ tema2/tema2-ex5.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.ion()
+
 func = lambda x : np.exp(x)*np.sin(x)
 derfw = lambda c, h : (func(c+h) - func(c))/h
 derexact = lambda x : np.exp(x) * (np.sin(x) + np.cos(x))
@@ -16,7 +18,7 @@ ax_abs.loglog(hs, err(x_0 * np.ones_like(hs), hs), "o", color = "tab:orange")
 ax_abs.set_xlabel(r"$h$")
 ax_abs.set_ylabel("Erro absoluto")
 
-fig_abs.savefig("tema2/tema2-ex5_abs.pdf", dpi = 300, bbox_inches = "tight")
+#fig_abs.savefig("tema2/tema2-ex5_abs.pdf", dpi = 300, bbox_inches = "tight")
 
 fig, ax = plt.subplots()
 
@@ -31,4 +33,6 @@ ax.set_xlabel(r"$x$")
 
 ax.legend(loc = "best")
 
-fig.savefig("tema2/tema2-ex5_funcs.pdf", dpi = 300, bbox_inches = "tight")
+#fig.savefig("tema2/tema2-ex5_funcs.pdf", dpi = 300, bbox_inches = "tight")
+
+plt.show()
