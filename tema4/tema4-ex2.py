34928dc36abd304fefc1086f8b81728b42f19eb2
Braisa <oterolemabrais@gmail.com>
2025-02-26 18:47:49 +0100

    Tema 4 Exercicio 2 alternativo

--- tema4/tema4-ex2.py
+++ tema4/tema4-ex2.py
@@ -14,3 +14,23 @@ for i, f in enumerate((f1,f2)):
     aprox += h*np.sum(np.fromiter((f(a + k*h) for k in range(1,n)), dtype = float))
 
     print(f"A integral I_{i+1} resulta {aprox:.7f}")
+
+# Alternativamente
+
+def trapecio_compuesto(f, a, b, n):
+
+    """
+    f : función a integrar
+    a, b : extremos do intervalo
+    n : número de subintervalos
+    """
+
+    h = (b-a)/n
+    aprox = (f(a) + f(b))*h/2
+    aprox += h*np.sum(np.fromiter((f(a + k*h) for k in range(1,n)), dtype = float))
+
+    return aprox
+
+print(f"Alternativamente, chamando a unha función que contén o cálculo.")
+print(f"A integral I_1 resulta {trapecio_compuesto(f1,a,b,n):.7f}")
+print(f"A integral I_2 resulta {trapecio_compuesto(f2,a,b,n):.7f}")
