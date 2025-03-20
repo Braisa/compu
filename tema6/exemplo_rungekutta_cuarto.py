0da93d2d074f89839f1824c40647a0fac5de21bb
Braisa <oterolemabrais@gmail.com>
2025-03-20 19:09:23 +0100

    Exemplos Euler e Runge Kutta

---
+++ tema6/exemplo_rungekutta_cuarto.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+origin = time.time()
+
+deriv = lambda x : np.sin(x)
+t0, x0 = 0, 1
+deltas = (1e-3, 1e-2, 1e-1)
+
+fig, ax = plt.subplots()
+colors = ("tab:orange", "tab:purple", "tab:blue")
+
+for k, (delta, c) in enumerate(zip(deltas, colors)):
+    
+    t, x = t0, x0
+    ax.plot(t, x, ".", color = c, label = f"{delta}")
+    
+    for i in range(int(1/delta)):
+
+        t += delta
+        k1 = delta*deriv(x)
+        k2 = delta*deriv(x + k1/2)
+        k3 = delta*deriv(x + k2/2)
+        k4 = delta*deriv(x + k3)
+        x += ((k1+k4)/2 + k2 + k3)/3
+
+        ax.plot(t, x, ".", color = c)
+
+ax.set_xlabel("t")
+ax.set_ylabel("x")
+
+ax.set_xlim(left = t0)
+
+ax.legend(loc = "best")
+
+fig.savefig("tema6/exemplo_rungekutta_cuarto.pdf", dpi = 300, bbox_inches = "tight")
+
+print(f"Tempo de execución : {time.time() - origin:.2f}")
