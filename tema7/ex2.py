68b78bd3f55f122fc1419c2f9bd4dcbd8931adf5
Braisa <oterolemabrais@gmail.com>
2025-04-03 19:04:35 +0200

    Tema 7 Exercicios 2 e 3 Mellora

--- tema7/ex2.py
+++ tema7/ex2.py
@@ -14,29 +14,24 @@ initial[9:11] = 20
 boundary_dirichlet = (lambda t : 15, lambda t : 5)
 boundary_neumann = (lambda t : 0, lambda t : 0)
 boundaries = (boundary_dirichlet, boundary_neumann)
+boundary_types = ("dirichlet", "neumann")
 
-def dirichlet_diffusion_step(t, T_curr, T_old, boundary):
+def diffusion_step(t, T_curr, T_old, boundary, boundary_type):
     T_new = np.zeros_like(T_curr)
 
     T_new[1:-1] = 2/3 * (-.5*T_old[1:-1] + 2*T_curr[1:-1] + alpha * delta_t / delta_x**2 * (T_curr[:-2] - 2*T_curr[1:-1] + T_curr[2:]))
     
-    T_new[0], T_new[-1] = boundary[0](t), boundary[-1](t)
-    return T_new
-
-def neumann_diffusion_step(t, T_curr, T_old, boundary):
-    T_new = np.zeros_like(T_curr)
-
-    T_new[1:-1] = 2/3 * (-.5*T_old[1:-1] + 2*T_curr[1:-1] + alpha * delta_t / delta_x**2 * (T_curr[:-2] - 2*T_curr[1:-1] + T_curr[2:]))
+    if boundary_type == "dirichlet":
+        T_new[0], T_new[-1] = boundary[0](t), boundary[-1](t)
+    elif boundary_type == "neumann":
+        T_new[0], T_new[-1] = T_new[1] - delta_x * boundary[0](t), T_new[-2] - delta_x * boundary[1](t)
     
-    T_new[0], T_new[-1] = T_new[1] - delta_x * boundary[0](t), T_new[-2] - delta_x * boundary[1](t)
     return T_new
-
-methods = (dirichlet_diffusion_step, neumann_diffusion_step)
 
 fig, axs = plt.subplots(1, 2)
 titles = ("Dirichlet", "Fluxo nulo")
 
-for _j, (boundary, method, ax, title) in enumerate(zip(boundaries, methods, np.ravel(axs), titles)):
+for _j, (boundary, boundary_type, ax, title) in enumerate(zip(boundaries, boundary_types, np.ravel(axs), titles)):
 
     T, T_old = initial, initial
     ax.plot(T, ls = "solid")
@@ -44,7 +39,7 @@ for _j, (boundary, method, ax, title) in enumerate(zip(boundaries, methods, np.r
     for t in range(t_steps):
 
         T_old = T.copy()
-        T = method(t, T, T_old, boundary)
+        T = diffusion_step(t, T, T_old, boundary, boundary_type)
 
         if t % 10 == 0:
             ax.plot(T, ls = "solid")
--- tema7/ex3.py
+++ tema7/ex3.py
@@ -14,38 +14,33 @@ initial[9:11] = 20
 boundary_dirichlet = (lambda t : 15, lambda t : 5)
 boundary_neumann = (lambda t : 0, lambda t : 0)
 boundaries = (boundary_dirichlet, boundary_neumann)
+boundary_types = ("dirichlet", "neumann")
 
-def dirichlet_diffusion_step(t, T, boundary):
+def diffusion_step(t, T, boundary, boundary_type):
     T_new = np.zeros_like(T)
 
     T_new[2:-2] = T[2:-2] + alpha * delta_t / delta_x**2 * (-1/12*(T[4:] + T[:-4]) + 4/3*(T[3:-1] + T[1:-3]) - 5/2*T[2:-2])
     
-    T_new[1], T_new[-2] = boundary[0](t), boundary[-1](t)
-    T_new[0], T_new[-1] = T_new[1], T_new[-2]
-    return T_new
-
-def neumann_diffusion_step(t, T, boundary):
-    T_new = np.zeros_like(T)
-
-    T_new[2:-2] = T[2:-2] + alpha * delta_t / delta_x**2 * (-1/12*(T[4:] + T[:-4]) + 4/3*(T[3:-1] + T[1:-3]) - 5/2*T[2:-2])
+    if boundary_type == "dirichlet":
+        T_new[1], T_new[-2] = boundary[0](t), boundary[-1](t)
+        T_new[0], T_new[-1] = T_new[1], T_new[-2]
+    elif boundary_type == "neumann":
+        T_new[1], T_new[-2] = T_new[2] - delta_x * boundary[0](t), T_new[-3] - delta_x * boundary[1](t)
+        T_new[0], T_new[-1] = T_new[1], T_new[-2]
     
-    T_new[1], T_new[-2] = T_new[2] - delta_x * boundary[0](t), T_new[-3] - delta_x * boundary[1](t)
-    T_new[0], T_new[-1] = T_new[1], T_new[-2]
     return T_new
-
-methods = (dirichlet_diffusion_step, neumann_diffusion_step)
 
 fig, axs = plt.subplots(1, 2)
 titles = ("Dirichlet", "Fluxo nulo")
 
-for _j, (boundary, method, ax, title) in enumerate(zip(boundaries, methods, np.ravel(axs), titles)):
+for _j, (boundary, boundary_type, ax, title) in enumerate(zip(boundaries, boundary_types, np.ravel(axs), titles)):
 
     T = initial
     ax.plot(T, ls = "solid")
 
     for t in range(t_steps):
 
-        T = method(t, T, boundary)
+        T = diffusion_step(t, T, boundary, boundary_type)
 
         if t % 10 == 0:
             ax.plot(T, ls = "solid")
